95cd45f51c2274a4d538c1b541e928374a08d8d9
victorgvargas <vvargas2013@gmail.com>
2023-11-23 02:33:06 +0000

    added pagination to history table

--- src/app/components/history-table/history-table.component.ts
+++ src/app/components/history-table/history-table.component.ts
@@ -1,6 +1,16 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 export interface NetBudgetRecord {
   id: string;
@@ -14,12 +24,31 @@ export interface NetBudgetRecord {
 @Component({
   selector: 'app-history-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './history-table.component.html',
   styleUrl: './history-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HistoryTableComponent {
+export class HistoryTableComponent implements OnInit, OnChanges, AfterViewInit {
   @Input() data: NetBudgetRecord[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   displayedColumns = ['budget', 'currency', 'region', 'familySize', 'city'];
+  dataSource!: MatTableDataSource<NetBudgetRecord>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<NetBudgetRecord>(this.data);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'] && this.paginator) {
+      this.dataSource.data = [...this.data];
+      this.paginator.length = this.data.length;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
 }
